9c13ebece28094dcf4432ae84694b67bc7d2b3e4
Jeroen de Jong <j6.de.jong@dictu.nl>
2019-10-09 15:21:47 +0200

    added DrupalStatusrapport class

---
+++ web/modules/custom/jeroens_soap_webservice/src/Soap/Types/DrupalStatusrapport.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\jeroens_soap_webservice\Soap\Types;
+
+/**
+ * DrupalStatusrapport Type.
+ */
+class DrupalStatusrapport {
+  /**
+   * Drupal.
+   *
+   * @var string
+   */
+  public $Status;
+
+}
