28aa8fccc79267097e2d8fbc36b90084eaedc534
Jeroen de Jong <j6.de.jong@dictu.nl>
2019-11-08 15:12:11 +0100

    added remove orphan expirement to probeersel module

---
+++ web/modules/custom/jeroen_probeersels/src/Form/RemoveOrphanParagraphsForm.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\jeroen_probeersels\Form;
+
+use Drupal\Core\Form\FormInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+class RemoveOrphanParagraphsForm implements FormInterface {
+
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+
+    $form['batch_size'] = array(
+      '#type' => 'number',
+      '#title' => t('Batchsize'),
+      '#required' => TRUE,
+      '#default_value' => 1000
+    );
+
+    $form['actions'] = array(
+      '#type' => 'actions',
+    );
+    $form['actions'] ['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Verwerk')
+    );
+    return $form;
+  }
+
+  public function getFormId(): string {
+    return 'jeroen_probeersels_removeorphanparagraphs';
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $batchSize = $form_state->getValue('batch_size');
+    removeOrphanParagraphs($batchSize);
+
+    $message = 'Batch removeOrphanParagraphs ' . date("d-m-Y_H:i:s") . ' with ' . $batchSize . ' was started.';
+
+    drupal_set_message($message);
+    \Drupal::logger('jeroen_probeersels')->info(t($message));
+  }
+
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    
+  }
+
+}
